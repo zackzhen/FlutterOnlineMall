a7e2267fa894ebd322340745c194db776c7087a7
zackzhengg <2954396430@qq.com>
2023-07-15 08:46:54 +0800

    添加原生页面调用

--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
@@ -5,7 +5,7 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity:FlutterActivity() {
+class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -17,13 +17,17 @@ class MainActivity:FlutterActivity() {
         // 为 channel 设置回调
         channel.setMethodCallHandler { call, res ->
             // 根据方法名，分发不同的处理
-            when(call.method) {
+            when (call.method) {
                 "your_method_name" -> {
                     // 获取传入的参数
                     val msg = call.argument<String>("msg")
                     Log.i("ZHP", "正在执行原生方法，传入的参数是：「$msg」")
                     // 通知执行成功
                     res.success("这是执行的结果")
+
+                    val intents = intent
+                    intents.setClass(context, NativeActivity::class.java)
+                    startActivity(intents)
                 }
 
                 else -> {
---
+++ android/app/src/main/kotlin/com/example/untitled/NativeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.untitled
+
+import android.app.Activity
+import android.os.Bundle
+
+class NativeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_native)
+    }
+}
